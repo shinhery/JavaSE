a90e5a1e97b54bf9aaa589447219f117e02c93c1
CaldWell63 <shinjuheon04@gmail.com>
2023-07-17 19:48:25 +0900

    Update Variable03.java

--- src/section02/Variable03.java
+++ src/section02/Variable03.java
@@ -1,10 +1,10 @@
 package section02;
 /*
  * 3. 정수형
- * 	byte 	(1byte)
- * 	short	(2byte)
- * 	int		(4byte)
- * 	long	(8byte)
+ * 	byte 	(1byte) : -128 ~ +127
+ * 	short	(2byte) : -32,768 ~ +32,767
+ * 	int		(4byte) : -2,147,483,648 ~ +2,147,483,647
+ * 	long	(8byte) : -9,223,372,036,854,775,808 ~ +9,223,372,036,854,775,807
  *  
  */
 public class Variable03 {
